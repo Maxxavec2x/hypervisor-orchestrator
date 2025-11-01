0067b937a8e8d374eec595a9b391efcd41cdbf28
Maxence SIBONI <maxencesibonibergard@gmail.com>
2025-11-01 18:01:53 +0100

    ajout d'un objet domainInfo, et d'un endpoint pour récupérer une liste des domains

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
 import json
-
+from pprint import pprint
 from functions.orchestrator_lib import *
 from objects.NodeInfo import NodeInfo
 app = Flask(__name__)
 
 # Permet de récupérer les infos d'un node
-@app.route("/node-info/<ip>", methods=['GET'])
+@app.route("/node/<ip>/info", methods=['GET'])
 def get_node_info_endpoint(ip):
     node_info_array = get_node_info(get_connector_to_node(ip))
     node_info=NodeInfo(node_info_array)
-    return json.dumps(node_info.__dict__)
+    print(node_info.__dict__)
+    return jsonify(node_info.__dict__)
 
 
+# Permet de récupérer les infos de tout les domaines
+@app.route("/node/<ip>/domains/info", methods=['GET'])
+def get_all_domains_info_endpoint(ip):
+    domainArray = get_all_domain_info(get_connector_to_node(ip))
+    domain_dicts = [domain.to_dict() for domain in domainArray]
+    return jsonify(domain_dicts)
 
 # Pour les tests
 if __name__ == '__main__':
-    get_node_info_endpoint("localhost")
+    app.run(debug=True)
+    #help(libvirt)
+    #get_node_info_endpoint("localhost")
+    pprint(get_all_domains_info_endpoint("localhost"))
+
