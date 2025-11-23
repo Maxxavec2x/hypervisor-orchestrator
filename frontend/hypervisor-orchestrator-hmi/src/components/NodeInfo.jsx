7e03be1d3b2ca68bb6235c3d68de904b3df02f46
Maxence SIBONI <maxencesibonibergard@gmail.com>
2025-11-23 21:48:41 +0100

    correction d'un bug qui faisait que la quantitée de ram dans le formulaire était multiplié par 1000 en cas d'erreur (oui oui vous avez bien lu)

--- frontend/hypervisor-orchestrator-hmi/src/components/NodeInfo.jsx
+++ frontend/hypervisor-orchestrator-hmi/src/components/NodeInfo.jsx
@@ -60,9 +60,6 @@ export const NodeInfo = ({ nodeIp }) => {
     try {
       const formData = new FormData();
       for (let key in form) {
-        if (key === "ram_allocated") {
-          form["ram_allocated"] *= 1000 // Un peu dégueu, mais l'api demande une quantitée de ram en KiB MDRRRRRRRR
-        }
         formData.append(key, form[key]);
       }
 
--- frontend/hypervisor-orchestrator-hmi/src/functions/createDomain.jsx
+++ frontend/hypervisor-orchestrator-hmi/src/functions/createDomain.jsx
@@ -1,6 +1,8 @@
 export async function CreateDomain(nodeIp, formData) {
   const API_URL = `http://localhost:5000/node/${nodeIp}/domains/defineXML`;
-
+  
+  // Je suis obligé de mettre ça parce que l'api back attend l'unité de ram en Kib MDR 
+  formData.set("ram_allocated", formData.get("ram_allocated") * 1000)
   const res = await fetch(API_URL, {
     method: "POST",
     body: formData
